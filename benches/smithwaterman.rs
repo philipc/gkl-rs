3893cf208daa0848edca5819fd876a33d8a7aa2a
Philip Craig <philipjcraig@gmail.com>
2021-12-22 14:51:27 +1000

    smithwaterman: add benchmark
    This uses the same data as pairhmm. Not sure if that is appropriate.

---
+++ benches/smithwaterman.rs
@@ -0,0 +1,58 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+criterion_main!(benches);
+criterion_group!(benches, bench);
+
+fn bench(c: &mut Criterion) {
+    let in_file = File::open("tests/data/10s.in").unwrap();
+    let mut in_lines = BufReader::new(in_file).lines();
+    let mut tests = Vec::new();
+    while let Some(line) = in_lines.next() {
+        let line = line.unwrap();
+        let mut tokens = line.split_whitespace();
+        let rs_num = tokens.next().unwrap().parse::<usize>().unwrap();
+        let hap_num = tokens.next().unwrap().parse::<usize>().unwrap();
+        let mut rs = Vec::new();
+        for _ in 0..rs_num {
+            let line = in_lines.next().unwrap().unwrap();
+            let mut tokens = line.split_whitespace();
+            let rs_bytes = tokens.next().unwrap().as_bytes().to_vec();
+            rs.push(rs_bytes);
+        }
+        let mut hap = Vec::new();
+        for _ in 0..hap_num {
+            hap.push(in_lines.next().unwrap().unwrap());
+        }
+        tests.push((rs, hap));
+    }
+
+    let parameters = gkl::smithwaterman::Parameters::new(1, -4, -6, -1);
+    macro_rules! bench {
+        ($f: expr) => {
+            for (rs, hap) in &tests {
+                for rs in rs.iter() {
+                    for hap in hap.iter() {
+                        $f(
+                            hap.as_bytes(),
+                            rs,
+                            parameters,
+                            gkl::smithwaterman::OverhangStrategy::Indel,
+                        )
+                        .unwrap();
+                    }
+                }
+            }
+        };
+    }
+
+    if let Some(f) = gkl::smithwaterman::align_i32x8() {
+        c.bench_function("align_i32x8", |b| b.iter(|| bench!(f)));
+    }
+
+    if let Some(f) = gkl::smithwaterman::align_i32x16() {
+        c.bench_function("align_i32x16", |b| b.iter(|| bench!(f)));
+    }
+}
